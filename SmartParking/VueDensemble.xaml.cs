66f3d9ea7a6385839834e67f6f0204d30fd3926b
Taha <taha.khelifi@saintmichelannecy.fr>
2025-03-13 10:49:14 +0100

    aze

--- SmartParking/VueDensemble.xaml.cs
+++ SmartParking/VueDensemble.xaml.cs
@@ -73,7 +73,6 @@ namespace SmartParking
 
                         TextLibre.Text = placesDisponibles.ToString();
                         TextOccupe.Text = placesOccupees.ToString();
-                        TextReserve.Text = placesReservees.ToString();
                         
                     }
                     else
